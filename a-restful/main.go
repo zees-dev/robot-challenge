89cb308db9ea58eeb9e554097ad531642f53d2cc
zees-dev <dev.786zshan@gmail.com>
2020-12-29 02:16:55 +1300

    configurable robot position intialisation co-ordinates via flags

--- a-restful/main.go
+++ a-restful/main.go
@@ -1,12 +1,24 @@
 package main
 
-import "log"
+import (
+	"flag"
+	"log"
+)
 
 func main() {
-	// TODO - initialise robot in x,y within 10 - using flags
-	// TODO - check if within bounds
+	xPtr := flag.Uint("x", 0, "robot initialisation x co-ordinate")
+	yPtr := flag.Uint("y", 0, "robot initialisation y co-ordinate")
+	xDimension, yDimension := uint(10), uint(10)
+	flag.Parse()
+
+	x, y := *xPtr, *yPtr
+	if x >= xDimension {
+		log.Fatalf("Invalid robot x position; x co-ordinate must satisfy 0 <= x < %d", xDimension)
+	}
+	if y >= yDimension {
+		log.Fatalf("Invalid robot x position; y co-ordinate must satisfy 0 <= y < %d", yDimension)
+	}
 	// TODO (improvement) - check if not being registered on an existing robos location (if multi-robots are supported)
-	x, y := uint(0), uint(0)
 
 	// db := NewDB()
 	// latestState, err := db.GetLatestState()
@@ -25,7 +37,7 @@ func main() {
 	// TODO serve OpenAPI spec from static file server
 	// TODO serve minimal frontend from static file server
 
-	robotServer(robot)
+	robotAPIServer(robot)
 
 	// TODO look into graceful server shutdown (OS signals)
 }
