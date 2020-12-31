70d6b4591efbebcece6f1b9da5f126d21cc9a4cd
zees-dev <dev.786zshan@gmail.com>
2020-12-31 14:41:34 +1300

    eventstream format is spec compliant

--- a-restful/api.go
+++ a-restful/api.go
@@ -8,7 +8,6 @@ import (
 	"log"
 	"net/http"
 	"strings"
-	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -164,51 +163,23 @@ func RobotAPIServer(robot *Bot) http.Handler {
 		w.Header().Set("Connection", "keep-alive")
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 
-		// instantiate the channel
-		// messageChan := make(chan string)
-
-		// go func() {
-		// 	for {
-		// 		time.Sleep(time.Second * 2)
-		// 		eventString := fmt.Sprintf("the time is %v", time.Now())
-		// 		log.Println("Receiving event")
-		// 		messageChan <- eventString
-		// 	}
-		// }()
-
-		// // trap the request under loop forever
-		// for {
-		// 	select {
-		// 	// message will received here and printed
-		// 	case message := <-messageChan:
-		// 		fmt.Fprintf(w, "%s\n", message)
-		// 		flusher.Flush()
-		// 	// connection is closed then defer will be executed
-		// 	case <-r.Context().Done():
-		// 		return
-		// 	}
-		// }
-
 		// enqueue empty task to hook into state and error channels
 		_, stateCh, errorsCh := robot.EnqueueTask("")
 
-		go func() {
-			for {
-				time.Sleep(time.Second * 2)
-				log.Println("Receiving event")
-				stateCh <- robot.CurrentState()
-			}
-		}()
-
+		// Event stream format/spec: https://developer.mozilla.org/en-US/docs/Web/API/Server-sent_events/Using_server-sent_events#Event_stream_format
 		for {
 			select {
 			case state := <-stateCh:
 				log.Printf("SSE recieving - robot state %v", state)
-				fmt.Fprintf(w, `{"x": %d, "y": %d}%s`, state.X, state.Y, "\n")
+				fmt.Fprint(w, "event: robotstate\n")
+				fmt.Fprintf(w, `data: {"x": %d, "y": %d}%s`, state.X, state.Y, "\n")
+				fmt.Fprint(w, "\n")
 				flusher.Flush()
 			case err := <-errorsCh:
 				log.Printf("SSE recieving - error %v", err)
-				fmt.Fprintf(w, `{"error": "%s"}%s`, err.Error(), "\n")
+				fmt.Fprint(w, "event: roboterror\n")
+				fmt.Fprintf(w, `data: %s%s`, err.Error(), "\n")
+				fmt.Fprint(w, "\n")
 				flusher.Flush()
 			case <-r.Context().Done():
 				return
