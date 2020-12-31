326962d6c053b1a524d7769cd70806cefb255a1f
zees-dev <dev.786zshan@gmail.com>
2020-12-31 14:04:12 +1300

    init frontend & SSE POC

--- a-restful/api.go
+++ a-restful/api.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"strings"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -144,6 +145,77 @@ func RobotAPIServer(robot *Bot) http.Handler {
 
 		w.WriteHeader(http.StatusNoContent)
 	}).Methods("DELETE")
+
+	// CHALLENGE
+	// HTTP2 SSE - realtime unidirectional communication
+	router.HandleFunc("/api/v1/state/subscribe", func(w http.ResponseWriter, r *http.Request) {
+		log.Println("established handshake with client...")
+
+		// ensure writer supports streaming
+		flusher, ok := w.(http.Flusher)
+		if !ok {
+			http.Error(w, "Streaming unsupported!", http.StatusInternalServerError)
+			return
+		}
+
+		// set SSE headers
+		w.Header().Set("Content-Type", "text/event-stream")
+		w.Header().Set("Cache-Control", "no-cache")
+		w.Header().Set("Connection", "keep-alive")
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		// instantiate the channel
+		// messageChan := make(chan string)
+
+		// go func() {
+		// 	for {
+		// 		time.Sleep(time.Second * 2)
+		// 		eventString := fmt.Sprintf("the time is %v", time.Now())
+		// 		log.Println("Receiving event")
+		// 		messageChan <- eventString
+		// 	}
+		// }()
+
+		// // trap the request under loop forever
+		// for {
+		// 	select {
+		// 	// message will received here and printed
+		// 	case message := <-messageChan:
+		// 		fmt.Fprintf(w, "%s\n", message)
+		// 		flusher.Flush()
+		// 	// connection is closed then defer will be executed
+		// 	case <-r.Context().Done():
+		// 		return
+		// 	}
+		// }
+
+		// enqueue empty task to hook into state and error channels
+		_, stateCh, errorsCh := robot.EnqueueTask("")
+
+		go func() {
+			for {
+				time.Sleep(time.Second * 2)
+				log.Println("Receiving event")
+				stateCh <- robot.CurrentState()
+			}
+		}()
+
+		for {
+			select {
+			case state := <-stateCh:
+				log.Printf("SSE recieving - robot state %v", state)
+				fmt.Fprintf(w, `{"x": %d, "y": %d}%s`, state.X, state.Y, "\n")
+				flusher.Flush()
+			case err := <-errorsCh:
+				log.Printf("SSE recieving - error %v", err)
+				fmt.Fprintf(w, `{"error": "%s"}%s`, err.Error(), "\n")
+				flusher.Flush()
+			case <-r.Context().Done():
+				return
+			}
+		}
+
+	}).Methods("GET")
 
 	return router
 }
